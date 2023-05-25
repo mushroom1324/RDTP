42b98fdb3a0d310f3496be89166fdbedfe54943c
seochanhyeok <mushroom1324@naver.com>
2023-05-25 20:07:40 +0900

    FEAT : implement rcv window display, and.. bunch of logics

--- RDTP/receiverY.py
+++ RDTP/receiverY.py
@@ -1,5 +1,9 @@
 import socket
+import threading
 import configparser
+import time
+import tkinter as tk
+from colorama import Fore
 
 config = configparser.ConfigParser()
 config.read('../RDTP.conf')
@@ -20,7 +24,7 @@ sender_port_number = int(config['DEFAULT']['sender_port_number'])
 
 application_storage = 0
 
-window_size = 256
+window_size = 170
 window = 0
 
 rcvbase = 0
@@ -51,50 +55,116 @@ while True:
     break
 
 """
-after connection is established
+after connection is established, show display
 """
 
+# kill thread event
+stop_event = threading.Event()
 
-while True:
-    print("rcvbase:", rcvbase, "last_byte_rcvd:", last_byte_rcvd, "last_byte_read:", last_byte_read, "window:", window)
-    # application read data
-    if last_byte_rcvd != last_byte_read:
-        print("Application read data:", application_storage, rcvbase)
-        application_storage += window
-        rcvbase += window
-        last_byte_read = last_byte_rcvd
-        window = 0
+# Create the main Tkinter window
+root = tk.Tk()
 
-    # receive message
-    bytesAddressPair = UDPReceiverSocket.recvfrom(window_size)
-    print("From Sender:", bytesAddressPair[1], "Message:", bytesAddressPair[0].decode())
-    if bytesAddressPair[0].decode() == "FIN":
-        # send ACK to sender
-        print("To Sender:", "ACK")
-        UDPReceiverSocket.sendto("ACK".encode(), bytesAddressPair[1])
-        if window:
-            print("Application read data:", application_storage, rcvbase)
+root.title('Receiver Y')
+
+# Create a canvas to draw the rectangles
+canvas = tk.Canvas(root, width=1024, height=200)
+canvas.pack()
+
+
+def draw_rectangles(rcvbase, last_byte_rcvd, last_byte_read, window):
+
+    print(Fore.GREEN + "rcvbase", rcvbase, "last_byte_rcvd", last_byte_rcvd, "last_byte_read", last_byte_read, "window", window)
+
+    for i in range(min(0, rcvbase - 10), 512):
+        color = "white"
+        if i <= rcvbase:
+            color = "grey"
+        elif i <= last_byte_rcvd:  # which cannot happen..
+            color = "blue"
+        elif i <= last_byte_read:
+            color = "green"
+        elif i <= rcvbase + window_size:
+            color = "black"
+        canvas.create_rectangle(i * 2, 0, (i + 1) * 2, 200, fill=color)
+
+def update_canvas(rcvbase, last_byte_rcvd, last_byte_read, window):
+    canvas.delete("all")
+    draw_rectangles(rcvbase, last_byte_rcvd, last_byte_read, window)
+
+
+
+"""
+
+receive message
+
+"""
+
+def read_message():
+    global rcvbase, last_byte_rcvd, last_byte_read, window, application_storage, root
+
+    while not stop_event.is_set():
+        # application read data
+        if last_byte_rcvd != last_byte_read:
             application_storage += window
             rcvbase += window
             last_byte_read = last_byte_rcvd
             window = 0
-        exit(0)
+            print(Fore.BLUE + "Application storage:", application_storage)
 
-    receivedMessage = bytesAddressPair[0].decode()[4:-1].split(", ")
-
-    if int(receivedMessage[0]) == last_byte_rcvd:
-        # message received in order
-        window += int(receivedMessage[1])
-
-        last_byte_rcvd += int(receivedMessage[1])
-        sendMessage = "ACK(" + str(last_byte_rcvd + 1) + ", " + str(window_size - window) + ")"
-        print("To Sender:", sendMessage)
-        UDPReceiverSocket.sendto(sendMessage.encode(), bytesAddressPair[1])
-    else:
-        # discard segment that is out of order
-        # resend ACK
-        sendMessage = "ACK(" + str(last_byte_rcvd + 1) + ", " + str(window_size - window) + ")"
-        print("To Sender (Retransmit):", sendMessage)
-        UDPReceiverSocket.sendto(sendMessage.encode(), bytesAddressPair[1])
+        time.sleep(1)
 
 
+def receive_message():
+    global rcvbase, last_byte_rcvd, last_byte_read, window, application_storage, root
+
+    while True:
+        # receive message
+        bytesAddressPair = UDPReceiverSocket.recvfrom(window_size)
+        print(Fore.RESET + "From Sender:", bytesAddressPair[1], "Message:", bytesAddressPair[0].decode())
+        if bytesAddressPair[0].decode() == "FIN":
+            # send ACK to sender
+            print(Fore.RESET + "To Sender:", "ACK")
+            UDPReceiverSocket.sendto("ACK".encode(), bytesAddressPair[1])
+            time.sleep(2)
+            # update canvas
+            update_thread = threading.Thread(target=update_canvas, daemon=True,
+                                             args=(rcvbase, last_byte_rcvd, last_byte_read, window))
+            update_thread.start()
+            # root.quit()
+            stop_event.set()
+            time.sleep(2)
+            print(Fore.GREEN + "Connection Finished")
+            exit(0)
+
+        receivedMessage = bytesAddressPair[0].decode()[4:-1].split(", ")
+
+        if int(receivedMessage[0]) == last_byte_rcvd + 1:
+            # message received in order
+
+            last_byte_rcvd += int(receivedMessage[1])
+            sendMessage = "ACK(" + str(last_byte_rcvd + 1) + ", " + str(window_size - window) + ")"
+            print(Fore.RESET + "To Sender:", sendMessage)
+            UDPReceiverSocket.sendto(sendMessage.encode(), bytesAddressPair[1])
+            window += int(receivedMessage[1])
+        else:
+            # discard segment that is out of order
+            # resend ACK
+            sendMessage = "ACK(" + str(last_byte_rcvd + 1) + ", " + str(window_size - window) + ")"
+            print(Fore.YELLOW + "To Sender (Retransmit):", sendMessage)
+            UDPReceiverSocket.sendto(sendMessage.encode(), bytesAddressPair[1])
+
+        # update canvas
+        update_thread = threading.Thread(target=update_canvas, daemon=True,
+                                         args=(rcvbase, last_byte_rcvd, last_byte_read, window))
+        update_thread.start()
+
+
+# read message thread
+read_thread = threading.Thread(target=read_message, daemon=True)
+read_thread.start()
+
+# start receive message thread
+receive_thread = threading.Thread(target=receive_message, daemon=True)
+receive_thread.start()
+
+root.mainloop()
--- RDTP/senderX.py
+++ RDTP/senderX.py
@@ -17,12 +17,13 @@ first = 0
 i = 0
 while i < len(scenario) - 1:
     if i % 2:
-        scenarios.append((first, scenario[i]))
+        scenarios.append([first, scenario[i], 0])
     else:
         first = scenario[i]
     i += 1
-scenarios.append((scenario[i], 0))
+scenarios.append([scenario[i], 0, 0])
 print(*scenarios)
+message_index = 0
 
 t = time.time()
 
@@ -42,6 +43,7 @@ window_size = int(config['DEFAULT']['sender_window_size'])
 sequence_number = int(config['DEFAULT']['sender_init_seq_no'])
 window = 0
 next_sequence_number = 0
+message = 0
 
 sendbase = 0
 last_byte_sent = 0
@@ -71,7 +73,7 @@ print("From Receiver:", receiverAddress, "Message:", message.decode())
 message = message.decode()[1:-1].split(", ")
 
 # Set initial AdvWindow
-AdvWindow = int(message[1])
+adv_window = int(message[1])
 
 """
 after connection is established, show display
@@ -82,9 +84,11 @@ stop_event = threading.Event()
 
 # Create the main Tkinter window
 root = tk.Tk()
+root.title('Sender X')
 
 # Create a canvas to draw the rectangles
 canvas = tk.Canvas(root, width=1024, height=200)
+
 canvas.pack()
 
 
@@ -116,53 +120,54 @@ def update_canvas(sendbase, last_byte_acked, last_byte_sent, last_byte_written):
 start receive
 
 """
+# Initialize timer
+timer = threading.Timer(timeout_value, update_canvas)
 
 
-def send_message(next_sequence_number, length, Timeout=False):
-    global AdvWindow, timer, last_byte_sent, last_byte_written, last_byte_acked, PrevByteAcked, t
+def send_message(sequence_number, length, is_timeout=False):
+    global adv_window, timer, last_byte_sent, last_byte_written, last_byte_acked, t, timeout_value
 
+    # print(next_sequence_number, last_byte_acked)
     if stop_event.is_set():
-        return
+        print(Fore.RED + "end process")
+        return 0
 
-    if Timeout:
-        print(Fore.YELLOW + "Timeout event")
-    print(Fore.BLUE + "Time", int(time.time() - t), "Segment size:", length)
+    print(Fore.BLUE + "Time", int(time.time() - t), "Segment size:", length, "Sequence Number:", sequence_number)
 
-    if length > AdvWindow:
-        print(Fore.RESET + "Not enough space from receiver buffer..", length, ">", AdvWindow)
-        timer = threading.Timer(0.5, send_message, args=(next_sequence_number, length))
-        timer.start()
+    message = "SND(" + str(sequence_number) + ", " + str(length) + ")"
+    print(Fore.RESET + "To Receiver:", message)
+    UDPSenderSocket.sendto(message.encode(), channel_address)
+    if not is_timeout:
+        last_byte_sent += length
+        adv_window -= length
+
+    M[sequence_number] = [length, time.time()]
+    if is_timeout:
+        return 0
     else:
-        message = "SND(" + str(next_sequence_number) + ", " + str(length) + ")"
-        print(Fore.RESET + "To Receiver:", message)
-        UDPSenderSocket.sendto(message.encode(), channel_address)
-        if not Timeout:
-            last_byte_sent += length
-            AdvWindow -= length
-        # (3) timeout event
-        timer = threading.Timer(timeout_value, send_message, args=(next_sequence_number, length, True))
-        timer.start()
+        return length
 
-    M[next_sequence_number] = [length, time.time()]
+
+def retransmit():
+    for message, delay, is_acked in scenarios:
+        if not is_acked:
+            print(Fore.RED + "Time", int(time.time() - t), "Retransmitting", last_byte_acked + 1, "to", last_byte_acked + message)
+            send_message(last_byte_acked + 1, message, True)
+            break
+    timer = threading.Timer(timeout_value, retransmit)
+    timer.start()
 
 
 def message_loop():
-    global window, last_byte_written, next_sequence_number, last_byte_acked, AdvWindow, sendbase, last_byte_sent, timer, timeout_value
-
-    message_index = 0
+    global window, last_byte_written, next_sequence_number, last_byte_acked, adv_window, sendbase, last_byte_sent, timer, timeout_value, message, message_index
 
     while message_index < len(scenarios) or last_byte_acked != last_byte_sent:
 
         if message_index < len(scenarios):
             # (1) get input message
-            message, delay = scenarios[message_index]
-
+            message, delay, is_acked = scenarios[message_index]
             # check buffer size capacity
-            if message > window_size:
-                print(Fore.BLACK + "Cannot send message more than 1024 bytes")
-                message_index += 1
-                continue
-            elif window + message > window_size:
+            if window + message > window_size:
                 print(Fore.BLACK + "Not enough space from sender buffer.. waiting")
                 time.sleep(0.5)
                 continue
@@ -171,55 +176,85 @@ def message_loop():
             window += message
             last_byte_written += message
 
+            while message > adv_window:
+                print(Fore.RESET + "Not enough space from receiver buffer..", message, ">", adv_window)
+                time.sleep(0.5)
+
             # (2) send message to receiver
-            # if NextSeqNum != LastByteWritten:  # which is always happens.. so just send message
-            send_message(next_sequence_number, message)
-            next_sequence_number += message
+            if next_sequence_number != last_byte_written:  # which is always happens.. so just send message
+                next_sequence_number += send_message(next_sequence_number, message)
 
-        # update canvas
-        update_thread = threading.Thread(target=update_canvas, daemon=True, args=(sendbase, last_byte_acked, last_byte_sent, last_byte_written))
-        update_thread.start()
-
-        # (4) receive ACK from receiver
-        bytesAddressPair = UDPSenderSocket.recvfrom(window_size)
-        receivedMessage = bytesAddressPair[0].decode()[4:-1].split(", ")
-
-        print(Fore.RESET + "From Receiver:", bytesAddressPair[1], "Message:", bytesAddressPair[0].decode())
-
-        AdvWindow = int(receivedMessage[1])  # set AdvWindow size
-
-        if int(receivedMessage[0]) > sendbase:
-            sendbase = int(receivedMessage[0])
-            last_byte_acked = int(receivedMessage[0]) - 1
-            print(Fore.RED + "last_byte_sent", last_byte_sent, "last_byte_acked:", last_byte_acked)
-            if last_byte_acked != last_byte_sent:
-                # timeout_value -= int(time.time() - M[last_byte_acked][1])
-                print(Fore.CYAN + "Timeout value:", timeout_value)
-                # change timer to new timeout value
-                timer.cancel()
-                # TODO : man.. why..
-                timer = threading.Timer(timeout_value, send_message, args=(last_byte_acked + 1, message + last_byte_sent - last_byte_acked))
+            # (3) timeout event
+            if not timer.is_alive():
+                timer = threading.Timer(timeout_value, retransmit)
                 timer.start()
-        timer.cancel()
+
+            # update canvas
+            update_thread = threading.Thread(target=update_canvas, daemon=True, args=(sendbase, last_byte_acked, last_byte_sent, last_byte_written))
+            update_thread.start()
 
         message_index += 1
         time.sleep(delay)
-    print("Finish sending all messages..")
+
+    print(Fore.YELLOW + "Finish sending all messages..")
     stop_event.set()
     # send FIN message to receiver
     UDPSenderSocket.sendto("FIN".encode(), channel_address)
-    # receive ACK from receiver
+
+
+def receive_ack():
+    global adv_window, sendbase, last_byte_acked, last_byte_sent, timer, message, root, timeout_value
+
     while True:
-        print("Waiting for ACK..")
-        message, receiverAddress = UDPSenderSocket.recvfrom(window_size)
-        if message.decode() == "ACK":
-            print("From Receiver:", receiverAddress, "Message:", message.decode())
-            print("Connection Finished")
-            quit(0)
+        # (4) receive ACK from receiver
+        bytes_address_pair = UDPSenderSocket.recvfrom(window_size)
+
+        # Finish receiving ACK when last ACK received
+        if len(bytes_address_pair[0].decode()) == 3:
+            print(Fore.RESET + "From Receiver:", receiverAddress, "Message:", bytes_address_pair[0].decode())
+            print(Fore.GREEN + "Connection Finished")
+            timer.cancel()
+            # root.quit()
+            exit(0)
+
+        received_message = bytes_address_pair[0].decode()[4:-1].split(", ")
+
+        print(Fore.RESET + "From Receiver:", bytes_address_pair[1], "Message:", bytes_address_pair[0].decode())
+
+        adv_window = int(received_message[1])  # set AdvWindow size
+
+        if int(received_message[0]) > sendbase:
+            i = 0
+            temp = 0
+            while temp != int(received_message[0]) - 1:
+                temp += scenarios[i][0]
+                scenarios[i][2] = 1
+                i += 1
+
+            sendbase = int(received_message[0])
+            last_byte_acked = int(received_message[0]) - 1
+            if last_byte_acked != last_byte_sent:
+                # change timer to new one
+                timer.cancel()
+                timer = threading.Timer(timeout_value - int(time.time() - M[last_byte_acked][1]), retransmit)
+                timer.start()
+
+            else:
+                # ACK received to last_byte_sent
+                timer.cancel()
+
+
+        # update canvas
+        update_thread = threading.Thread(target=update_canvas, daemon=True,
+                                         args=(sendbase, last_byte_acked, last_byte_sent, last_byte_written))
+        update_thread.start()
 
 
 message_thread = threading.Thread(target=message_loop)
 message_thread.start()
+
+ack_thread = threading.Thread(target=receive_ack)
+ack_thread.start()
 
 root.mainloop()
 
